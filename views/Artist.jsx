cf5503935b1e013c931626f4cdc173281e45fb9c
evmac <macgregor.evan@gmail.com>
2017-09-11 22:59:31 -0400

    Adding content to views

---
+++ views/Artist.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Artist extends React.Component {
+  render() {
+    return (
+      <Album />
+    )
+  }
+}
+
+export default Artist
---
+++ views/Library.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Library extends React.Component {
+  render() {
+    return (
+      <Artist />
+    )
+  }
+}
+
+export default Library
